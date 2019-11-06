03029fc9216096ae87a8bcd7f3a78b0202c404a2
ginhart <43789376+ginhart@users.noreply.github.com>
2019-11-05 21:19:02 -0500

    Login in Angular

---
+++ AngularPart/proyectoNuevas/src/app/Components/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+    $(document).ready(function () {
+      $('.login-info-box').fadeOut();
+      $('.login-show').addClass('show-log-panel');
+    });
+
+
+    $('.login-reg-panel input[type="radio"]').on('change', function () {
+      if ($('#log-login-show').is(':checked')) {
+        $('.register-info-box').fadeOut();
+        $('.login-info-box').fadeIn();
+
+        $('.white-panel').addClass('right-log');
+        $('.register-show').addClass('show-log-panel');
+        $('.login-show').removeClass('show-log-panel');
+
+      }
+      else if ($('#log-reg-show').is(':checked')) {
+        $('.register-info-box').fadeIn();
+        $('.login-info-box').fadeOut();
+
+        $('.white-panel').removeClass('right-log');
+
+        $('.login-show').addClass('show-log-panel');
+        $('.register-show').removeClass('show-log-panel');
+      }
+    });
+
+  }
+
+}
